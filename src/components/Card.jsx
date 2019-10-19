8c386a12810514fe5869a33d027d0b88ea0e49fa
nomad302 <sagar.nargundi@gmail.com>
2019-10-19 16:55:27 +0530

    code refactoring done

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Card = props => {
+const Card = ({ name, email, id }) => {
   return (
     <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-      <img alt="robots" src={`https://robohash.org/${props.id}?50*50`} />
+      <img alt="robots" src={`https://robohash.org/${id}?50*50`} />
       <div>
-        <h2>{props.name}</h2>
-        <p>{props.email}</p>
+        <h2>{name}</h2>
+        <p>{email}</p>
       </div>
     </div>
   );
