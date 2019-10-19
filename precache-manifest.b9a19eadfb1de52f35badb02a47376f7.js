041a7f9966d21bae3b377b8ebd8f68a40d486a0e
nomad302 <sagar.nargundi@gmail.com>
2019-10-19 17:51:51 +0530

    Updates

---
+++ precache-manifest.b9a19eadfb1de52f35badb02a47376f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5101c8f049c9c1b33c6a7569181fca63",
+    "url": "/Robosearch/index.html"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/Robosearch/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "fb383fe8cbb8a42f56ff",
+    "url": "/Robosearch/static/css/main.2fe08ecb.chunk.css"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/Robosearch/static/js/2.fbad7c07.chunk.js"
+  },
+  {
+    "revision": "fb383fe8cbb8a42f56ff",
+    "url": "/Robosearch/static/js/main.c8db1bd5.chunk.js"
+  },
+  {
+    "revision": "8f4f6ce30c84d04418ef",
+    "url": "/Robosearch/static/js/runtime-main.8ebcf444.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robosearch/static/media/SEGA.12a3db8b.woff"
+  }
+]);
